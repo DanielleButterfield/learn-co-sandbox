ca06e9e0da8d110c187539a05d80549ec2b78521
Danielle Butterfield <butterfieldduncan@gmail.com>
2019-08-29 15:53:05 -0600

    Done.

--- Dogpark/environment.rb
+++ Dogpark/environment.rb
@@ -4,9 +4,12 @@ require_relative "./pet"
 
 danielle = Owner.new "Danielle", "Transfeminine Non-Binary"
 kevin = Owner.new "Kevin", "Cisgender Male"
+patt = Owner.new "Patt", "Cisgender Female"
 
 raven = Pet.new "Raven", "Black Lab", danielle.name
 fable = Pet.new "Fable", "Ausralian Shepherd Mix", danielle.name
 jack = Pet.new "Jack", "Pitbull Mix", kevin.name
+sam = Pet.new "Sam", "Parakeet", patt.name
+dean = Pet.new "Dean", "Parakeet", patt.name
 
 binding.pry
