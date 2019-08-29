99c556cc769bfbc88d0af6ac5b0cda0fa0ce7a25
Danielle Butterfield <butterfieldduncan@gmail.com>
2019-08-29 11:47:59 -0600

    Done.

--- SolarSystem/system.rb
+++ SolarSystem/system.rb
@@ -1,6 +1,27 @@
 require 'pry'
 require './planet'
 
-pluto = Planet.new("Pluto", "Celestial Dwarf", 9)
+pluto = Planet.new("Pluto", 1, 9)
+mercury = Planet.new("Mercury", 2, 3)
+earth = Planet.new("Earth", 4, 3)
+jupiter = Planet.new("Jupiter", 9, 3)
+
+def all_planets
+    planet_array = []
+    Planet.all.each do |planet|
+        planet_array << planet.name
+    end
+    planet_array
+end
+
+def planet_size
+    planet_array = []
+    Planet.all.each do |planet|
+        if planet.size.to_i >= 7
+            planet_array << planet.name
+        end
+    end
+    planet_array
+end
 
 binding.pry
